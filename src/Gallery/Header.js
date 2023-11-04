49cb3f6d9ac02e47691ae774bf06bcf6fd2c4d3d
Niloy-Dey <niloydeyce@gmail.com>
2023-11-04 22:06:47 +0600

    design and implementation done

---
+++ src/Gallery/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Header = ({ totalNumber, deleteImage, isChecked, toggleAllImages }) => {
+    console.log("totalNumber", totalNumber)
+    return (
+        <div>
+            <section>
+                {totalNumber > 0 ? 
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <label className='font-bold text-xl '>
+                                <input
+                                    className='mr-2 w-4 h-4'
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    onChange={toggleAllImages}
+                                />
+                                {totalNumber} File Selected
+                            </label>
+                        </div>
+
+                        <div>
+                            <button onClick={deleteImage} className='text-red-500 font-bold'>Delete Files</button>
+                        </div>
+                    </div>
+                 : 
+                    <h3 className='text-xl font-bold'>Gallery</h3>
+                }
+            </section>
+        </div>
+    );
+};
+
+export default Header;
